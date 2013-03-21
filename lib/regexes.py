78598740364b3b75ece5980c918fa925b905bb37
Jordan Wright <jmwright798@gmail.com>
2013-03-21 00:06:07 -0500

    Removed 16bytes hashes (don't seem very useful, and likely not going to be found) Improved DB regex

--- lib/regexes.py
+++ lib/regexes.py
@@ -3,7 +3,6 @@ import re
 regexes = {
 	'email' : re.compile(r'[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}', re.I),
 	#'ssn' : re.compile(r'\d{3}-?\d{2}-?\d{4}'),
-	'hash16' : re.compile(r'[A-E0-9]{16}', re.I),
 	'hash32' : re.compile(r'[A-E0-9]{32}', re.I),
 	'FFF' : re.compile(r'^(?:color:\s*)(?:#FFF)|(?:FBI\s*Friday)', re.I),
 	'lulz' : re.compile(r'(?:lulzsec)|(?:antisec)', re.I),
@@ -13,8 +12,8 @@ regexes = {
 	'db_leak' : [
 					re.compile(r'((?:users?)|(?:members?)|(?:accounts?)[-_|/\s]?(?:names?)|(?:ids?)\W)', re.I),
 					re.compile(r'target\s*?:?\s*?([a-z][\w-]+:/{1,3}(?:[-\w\s_]\.)*)', re.I) , # very basic URL check - may be improved later
-					re.compile(r'((?:mysql)|(?:sql\s*server))', re.I)
-					re.compile(r'(database)', re.I),
+					re.compile(r'((?:my\s?sql)|(?:sql\s*server))', re.I),
+					re.compile(r'((?:data[-_\s]*base)|(?:db))', re.I),
 					re.compile(r'(table)', re.I)
 				]
 }
